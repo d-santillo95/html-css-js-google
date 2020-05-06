a035b00fa0a38ec34514910d7fa4cc8ffd61f963
Daniele Santillo <daniele.santillo95@gmail.com>
2020-05-06 16:32:30 +0100

    aggiunto click per il menu-dropdown

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+$(document).click(function(e) {
+    if (!$(e.target).hasClass('nthm')) {
+        $('.container-dropdown-menu').removeClass('active-menu');
+    }
+})
+
 $('.dropdown').click(function(){
-    $('.container-dropdown-menu').toggle();
+    $('.container-dropdown-menu').toggleClass('active-menu');
 })
